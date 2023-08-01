19fd92cbc47f30abd4638a71a3b090be87dc31e5
NovusTheory <novustheory@gmail.com>
2023-07-31 22:13:30 -0500

    Finalize WCO titlebar

--- src/shared/components/TitleBar.vue
+++ src/shared/components/TitleBar.vue
@@ -15,7 +15,14 @@ defineProps({
   hasMaximizeButton: Boolean
 });
 
+const minimizeWindow = window.ytmd.minimizeWindow;
+const maximizeWindow = window.ytmd.maximizeWindow;
+const restoreWindow = window.ytmd.restoreWindow;
+const closeWindow = window.ytmd.closeWindow;
+
 const openSettingsWindow = window.ytmd.openSettingsWindow;
+
+const wcoVisible = ref((window.navigator as any).windowControlsOverlay.visible);
 
 const windowMaximized = ref(false);
 
@@ -36,6 +43,20 @@ window.ytmd.handleWindowEvents((event, state) => {
       <div class="app-buttons">
         <button v-if="hasSettingsButton" class="app-button" @click="openSettingsWindow" tabindex="1">
           <span class="material-symbols-outlined">settings</span>
+        </button>
+      </div>
+      <div v-if="!wcoVisible" class="windows-action-buttons">
+        <button v-if="hasMinimizeButton" class="action-button window-minimize" tabindex="2" @click="minimizeWindow">
+          <span class="material-symbols-outlined">remove</span>
+        </button>
+        <button v-if="hasMaximizeButton && !windowMaximized" class="action-button window-maximize" tabindex="3" @click="maximizeWindow">
+          <span class="material-symbols-outlined">square</span>
+        </button>
+        <button v-if="hasMinimizeButton && windowMaximized" class="action-button window-restore" tabindex="4" @click="restoreWindow">
+          <span class="material-symbols-outlined">filter_none</span>
+        </button>
+        <button class="action-button window-close" tabindex="5" @click="closeWindow">
+          <span class="material-symbols-outlined">close</span>
         </button>
       </div>
     </div>
@@ -137,5 +158,18 @@ window.ytmd.handleWindowEvents((event, state) => {
 
 .action-button > .material-symbols-outlined {
   font-size: 24px;
+}
+
+.windows-action-buttons {
+  display: flex;
+  margin-left: 8px;
+}
+
+.window-restore > .material-symbols-outlined {
+  transform: rotate(180deg);
+}
+
+.window-close:hover {
+  background-color: #e81123;
 }
 </style>
